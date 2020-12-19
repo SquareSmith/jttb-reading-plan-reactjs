105f9c5ff55da2852df26b3728a5c2a1639a68c2
SquareSmith <76259459+SquareSmith@users.noreply.github.com>
2020-12-19 14:14:06 -0600

    Use react-window FixedSizeList Use FixedSizeList to render the day's readings. The readings are hard coded for now.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,44 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import DatePicker from "react-datepicker";
+import { FixedSizeList as List } from "react-window";
 import useWindowSize from "./use-window-size";
 
 import "react-datepicker/dist/react-datepicker.css";
 import "./styles.css";
+
+const rowHeight = 35;
+
+const readings = [
+  {
+    date: "0413",
+    text: "1 Samuel 18-20",
+    url: "https://www.bible.com/bible/59/1SA.18"
+  },
+  {
+    date: "0413",
+    text: "Psalms 11",
+    url: "https://www.bible.com/bible/59/PSA.11"
+  },
+  {
+    date: "0413",
+    text: "Psalms 59",
+    url: "https://www.bible.com/bible/59/PSA.59"
+  }
+];
+
+const Row = ({ index, style }) => (
+  <div className={index % 2 ? "ListItemOdd" : "ListItemEven"} style={style}>
+    <a
+      className="bible-link"
+      href={readings[index].url}
+      rel="noreferrer"
+      target="_blank"
+    >
+      {readings[index].text}
+    </a>
+  </div>
+);
 
 const App = () => {
   const size = useWindowSize();
@@ -30,27 +64,15 @@ const App = () => {
         customInput={<CustomInput />}
         withPortal
       />
-      <a
-        className="bible-link"
-        href="https://www.bible.com/bible/59/JHN.1"
-        target="_blank"
+      <List
+        className="List"
+        height={readings.length * rowHeight}
+        itemCount={readings.length}
+        itemSize={rowHeight}
+        width={300}
       >
-        John 1
-      </a>
-      <a
-        className="bible-link"
-        href="https://www.bible.com/bible/59/JHN.2"
-        target="_blank"
-      >
-        John 2
-      </a>
-      <a
-        className="bible-link"
-        href="https://www.bible.com/bible/59/JHN.3"
-        target="_blank"
-      >
-        John 3
-      </a>
+        {Row}
+      </List>
     </div>
   );
 };
