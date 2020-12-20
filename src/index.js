52c4176da84648bfe3974f139a03758415548f5e
SquareSmith <76259459+SquareSmith@users.noreply.github.com>
2020-12-19 22:06:00 -0600

    Use the reading plan in jttb.csv Use Papa Parse to convert the local file jttb.csv into JSON data and use this data to drive the app.

--- src/index.js
+++ src/index.js
@@ -3,98 +3,65 @@ import ReactDOM from "react-dom";
 import DatePicker from "react-datepicker";
 import { FixedSizeList as List } from "react-window";
 import format from "date-fns/format";
-import useWindowSize from "./use-window-size";
-
 import "react-datepicker/dist/react-datepicker.css";
+import useWindowSize from "./use-window-size";
+import Papa from "papaparse";
+import readingsString from "../jttb.csv";
+
 import "./styles.css";
 
 const rowHeight = 35;
 
-const readingsAll = [
-  {
-    date: "0413",
-    text: "1 Samuel 18-20",
-    url: "https://www.bible.com/bible/59/1SA.18"
-  },
-  {
-    date: "0413",
-    text: "Psalms 11",
-    url: "https://www.bible.com/bible/59/PSA.11"
-  },
-  {
-    date: "0413",
-    text: "Psalms 59",
-    url: "https://www.bible.com/bible/59/PSA.59"
-  },
-  {
-    date: "0414",
-    text: "1 Samuel 21-24",
-    url: "https://www.bible.com/bible/59/1SA.21"
-  },
-  {
-    date: "0415",
-    text: "Psalms 7",
-    url: "https://www.bible.com/bible/59/PSA.7"
-  },
-  {
-    date: "0415",
-    text: "Psalms 27",
-    url: "https://www.bible.com/bible/59/PSA.27"
-  },
-  {
-    date: "0415",
-    text: "Psalms 31",
-    url: "https://www.bible.com/bible/59/PSA.31"
-  },
-  {
-    date: "0415",
-    text: "Psalms 34",
-    url: "https://www.bible.com/bible/59/PSA.34"
-  },
-  {
-    date: "0415",
-    text: "Psalms 52",
-    url: "https://www.bible.com/bible/59/PSA.52"
-  }
-];
-
-const Row = ({ index, style }) => (
-  <div className={index % 2 ? "ListItemOdd" : "ListItemEven"} style={style}>
-    <a
-      className="bible-link"
-      href={readingsAll[index].url}
-      rel="noreferrer"
-      target="_blank"
-    >
-      {readingsAll[index].text}
-    </a>
-  </div>
-);
+const results = Papa.parse(readingsString, {
+  delimiter: ",",
+  header: true
+});
+const readingsAll = results.data;
 
 const App = () => {
   const size = useWindowSize();
+
+  const readingsForDate = (date) => {
+    const dateKey = format(date, "MMdd");
+    return readingsAll.filter((reading) => reading.date === dateKey);
+  };
+
   const [date, setDate] = useState(new Date());
+  const [readings, setReadings] = useState(readingsForDate(date));
 
-  const dateKey = format(date, "MMdd");
-  console.log("Current dateKey is " + dateKey);
+  const setReadingsByDate = (date) => {
+    setDate(date);
+    setReadings(readingsForDate(date));
+  };
 
-  const readings = readingsAll.filter((reading) => reading.date === dateKey);
-
-  useEffect(() => {
-    document.title = "JTTB Daily Reading";
-  });
+  const Row = ({ index, style }) => (
+    <div className="ListItem" style={style}>
+      <a
+        className="bible-link"
+        href={readings[index].url}
+        rel="noreferrer"
+        target="_blank"
+      >
+        {readings[index].text}
+      </a>
+    </div>
+  );
 
   const CustomInput = ({ value, onClick }) => (
     <button className="custom-input" onClick={onClick}>
       {value}
     </button>
   );
+
+  useEffect(() => {
+    document.title = "JTTB Daily Reading";
+  });
+
   return (
     <div className="container" width={size.width} height={size.height}>
-      {size.width}px / {size.height}px
       <DatePicker
         selected={date}
-        onChange={(date) => setDate(date)}
+        onChange={(date) => setReadingsByDate(date)}
         dateFormat="MMMM d"
         todayButton="Today"
         customInput={<CustomInput />}
